0e8def638640a01ab58bd24b17f9ca4b6e7d83fc
lutfisky <ruben@webcontact.de>
2025-11-26 14:24:36 +0100

    push änderungen

--- dist/services/SensorSimulator.js
+++ dist/services/SensorSimulator.js
@@ -37,6 +37,49 @@ class SensorSimulator {
         if (table.plantSize <= 0) { // plants are dead
             return table;
         }
+        //   const temperature = table.temperature;
+        // const soilMoisture = table.soilMoisture;
+        // const watered = table.water;
+        // const fertilized = table.fertilizer;
+        // if (temperature > 60) {
+        //   table.plantSize = 0;
+        //   return table;
+        // }
+        // if (temperature >= 5 && temperature <= 40) {
+        //   const optimalTemp = 22;
+        //   const tempFactor = 1 - Math.abs(optimalTemp - temperature) / (optimalTemp - 5);
+        //   let moistureFactor = 0;
+        //   if (soilMoisture >= 50 && soilMoisture <= 80) {
+        //     moistureFactor = 1;
+        //   } else if (soilMoisture > 80) {
+        //     moistureFactor = 0.8;
+        //   } else {
+        //     moistureFactor = 0;
+        //   }
+        //   let fertilizerFactor = 1;
+        //   if (fertilized) {
+        //     if (watered) {
+        //       fertilizerFactor = 1.2;
+        //     } else {
+        //       table.plantSize = 0;
+        //       return table;
+        //     }
+        //   }
+        //   const growthRate = 0.01;
+        //   const growth = growthRate * tempFactor * moistureFactor * fertilizerFactor;
+        //   table.plantSize += growth;
+        //   if (this.logging) {
+        //     console.log(`Table ${table.position} growth: +${growth.toFixed(4)}, size: ${table.plantSize.toFixed(3)}`);
+        //   }
+        // } else {
+        //   if (this.logging) {
+        //     console.log(`Table ${table.position} no growth due to temperature ${temperature}°C`);
+        //   }
+        // }
+        // if (table.plantSize > 100) {
+        //   table.plantSize = 100;
+        // }
+        // return table;
         table.plantSize += 0.001;
         if (this.logging) {
             console.log(`Table ${table.position} plant size: ${table.plantSize.toFixed(3)}`);
